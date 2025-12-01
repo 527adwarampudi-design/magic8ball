78a53b5d40d89c65ca41fe0b40b9c93fe7ea3393
527adwarampudi-design <527adwarampudi@frhsd.com>
2025-12-01 15:40:16 +0000

    changes6

--- 8ball.js
+++ 8ball.js
@@ -1,3 +1,7 @@
 function shakeMagic8Ball(){
-    prompt("What is your question for the magic 8 Ball?")
+   const question=prompt("What is your question for the magic 8 Ball?");
+   
+   if(question === null){
+ document.getElementById('response-text').innerText='Please Ask a Question!!'
+   }
 }
